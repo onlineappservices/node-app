f3b8bfcda06c33027f83490bc6828398d0121109
Keegan Donley <keegan.donley@gmail.com>
2016-01-21 10:13:23 -0800

    update pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/help', function(req, res, next) {
+  res.render('help', { title: 'Help' });
+});
+
+router.get('/faq', function(req, res, next) {
+  res.render('faq', { title: 'FAQ' });
 });
 
 module.exports = router;
